ab05a073280cedc4aa808073c83d8f06a2f061d6
Arnab Ghosh <arnabg@ttic.edu>
2017-03-16 22:27:32 +0530

    can be parallelized

--- multi_circle_classifying_gen.lua
+++ multi_circle_classifying_gen.lua
@@ -1,27 +1,27 @@
 require 'torch'
 require 'nn'
-
 require 'optim'
 
 local model_utils=require 'util.model_utils'
 require 'image'
 require 'gnuplot'
 opt={
-    ncircles=1,
-    distC=10,
+    ncircles=3,
+    distC=100,
     ngen=3,
-    ndata=100000,             -- number of batches per epoch
+    nz=3,
+    batchSize=128,
+    R=5,
     ncentres=6,
+    
+    ndata=100000,             -- number of batches per epoch
     std_dev=0.1,
     lr = 0.0002,            -- initial learning rate for adam
     beta1 = 0.5,            -- momentum term of adam
-    nz=3,
-    batchSize=512,
     ndim=2,
-    R=5,
     nvis=3,                    -- Number of samples to be visualized
     save_freq=1,
-    exp_name='circles3gen/',
+    exp_name='circlesGen',
     niter=1200,
     batchnorm=true
 }
@@ -31,12 +31,12 @@ G={}
 for k,v in pairs(opt) do opt[k] = tonumber(os.getenv(k)) or os.getenv(k) or opt[k] end
 print(opt)
 
-
 opt.manualSeed = torch.random(1, 10000) -- fix seed
 print("Random Seed: " .. opt.manualSeed)
 torch.manualSeed(opt.manualSeed)
 torch.setnumthreads(1)
 torch.setdefaulttensortype('torch.FloatTensor')
+
 ngen=opt.ngen
 ndata=opt.ndata
 ncentres=opt.ncentres
@@ -48,6 +48,7 @@ nvis=opt.nvis
 save_freq=opt.save_freq
 distC=opt.distC
 ncircles=opt.ncircles
+batchSize=opt.batchSize
 local real_label=ngen+1
 local fake_labels=torch.linspace(1,ngen,ngen)
 
@@ -64,10 +65,9 @@ G.netG1:add(nn.ReLU())
 G.netG1:add(nn.Linear(128,128))
 if opt.batchnorm then
     G.netG1:add(nn.BatchNormalization(128))    
-end 
+end
 G.netG1:add(nn.ReLU())
 G.netG1:add(nn.Linear(128,ndim))
-
 
 for i=2,ngen do
     G['netG'..i]=G.netG1:clone()
@@ -108,15 +108,12 @@ local fDx=function(x)
         real=input:normal(0,std_dev)
         for j=1,opt.batchSize do
             k=randints[j]
-            --print('k '..k)
-            --print('x: value '..tostring(R*math.cos((2*k*math.pi)/ncentres)))
-            --print('y: value '..tostring(R*math.sin((2*k*math.pi)/ncentres)))
             real[j][1]=torch.normal(0,std_dev)+R*math.cos((2*k*math.pi)/ncentres)
             real[j][2]=torch.normal(0,std_dev)+R*math.sin((2*k*math.pi)/ncentres)
             k=randshifts[j]
-            --real[j][1]=real[j][1]+distC*math.cos((2*k*math.pi)/ncircles)
-            --real[j][2]=real[j][2]+distC*math.sin((2*k*math.pi)/ncircles)
-       end
+            real[j][1]=real[j][1]+distC*math.cos((2*k*math.pi)/ncircles)
+            real[j][2]=real[j][2]+distC*math.sin((2*k*math.pi)/ncircles)
+        end
         input:copy(real)
         --print(input)
         label:fill(real_label)
@@ -167,12 +164,13 @@ for epoch=1,opt.niter do
         real[j][1]=torch.normal(0,std_dev)+R*math.cos((2*k*math.pi)/ncentres)
         real[j][2]=torch.normal(0,std_dev)+R*math.sin((2*k*math.pi)/ncentres)
         k=randshifts[j]
-        --real[j][1]=real[j][1]+distC*math.cos((2*k*math.pi)/ncircles)
-        --real[j][2]=real[j][2]+distC*math.sin((2*k*math.pi)/ncircles)
+        real[j][1]=real[j][1]+distC*math.cos((2*k*math.pi)/ncircles)
+        real[j][2]=real[j][2]+distC*math.sin((2*k*math.pi)/ncircles)
     end
     if epoch%save_freq==0 then
-        paths.mkdir(opt.exp_name..tostring(epoch))
-        file=io.open('gaussians.txt','w')
+        local dir = opt.exp_name..'_'..tostring(ncircles)..'_'..tostring(distC)..'_'..tostring(ngen)..'_'..tostring(nz)..'_'..tostring(batchSize)..'_'..tostring(R)..'_'..tostring(ncentres)..'/'..tostring(epoch)
+        paths.mkdir(dir)
+        file=io.open(dir..'/out.txt','w')
         for i=1,ngen do
             for j=1,nvis do
                 noise=noise:normal(0,1)
@@ -185,26 +183,26 @@ for epoch=1,opt.niter do
             end
         end
         io.close(file)
-        gnuplot.pngfigure(opt.exp_name ..tostring(epoch)..'/out.png' )
-        gnuplot.raw("plot 'gaussians.txt' using 2:3:(sprintf('%d', $1)) with labels point pt 7 offset char 0.5,0.5 notitle")
+        gnuplot.pngfigure(dir..'/out.png' )
+        gnuplot.raw("plot '"..dir..'/out.txt'.."' using 2:3:(sprintf('%d', $1)) with labels point pt 7 offset char 0.5,0.5 notitle")
         gnuplot.grid(true)
         --gnuplot.scatter3(torch.zeros(nvis*opt.batchSize)  , vis[{{1,nvis*opt.batchSize},1}] ,  vis[{{1,nvis*opt.batchSize},2}]  )
         --gnuplot.scatter3( vis[{{1,nvis*opt.batchSize},1}] ,  vis[{{1,nvis*opt.batchSize},2}] , torch.zeros(nvis*opt.batchSize)  )
         gnuplot.plotflush()
         gnuplot.close()
         
-        inp_file=io.open('input.txt','w')
+        inp_file=io.open(dir..'/input.txt','w')
         io.output(inp_file)
         for k=1,opt.batchSize do
             io.write(string.format('%d %f %f\n',0,real[k][1],real[k][2]))
         end
         io.close(inp_file)
-        gnuplot.pngfigure(opt.exp_name ..tostring(epoch)..'/input.png' )
-        gnuplot.raw("plot 'input.txt' using 2:3:(sprintf('%d', $1)) with labels point pt 7 offset char 0.5,0.5 notitle")
+        gnuplot.pngfigure(dir..'/input.png' )
+        gnuplot.raw("plot '"..dir..'/input.txt'.."' using 2:3:(sprintf('%d', $1)) with labels point pt 7 offset char 0.5,0.5 notitle")
         gnuplot.grid(true)
         --gnuplot.scatter3(torch.zeros(nvis*opt.batchSize)  , vis[{{1,nvis*opt.batchSize},1}] ,  vis[{{1,nvis*opt.batchSize},2}]  )
         --gnuplot.scatter3( vis[{{1,nvis*opt.batchSize},1}] ,  vis[{{1,nvis*opt.batchSize},2}] , torch.zeros(nvis*opt.batchSize)  )
         gnuplot.plotflush()
-        gnuplot.close() 
+        gnuplot.close()
     end
 end
